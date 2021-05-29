d5a1b4934b6fb497451e9425612f4dcbaccedd80
aaronh1986 <aaronharbourne@gmail.com>
2021-05-29 19:41:25 +0000

    Check to make sure exactly six values have been input. If not display a warning

--- run.py
+++ run.py
@@ -12,8 +12,32 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
+def get_sales_data():
+    """
+    Get sales figures input from the user
+    """
+    print("Please enter sales figures from the last market day")
+    print("Input should be in the form 10, 20, 30, 40, 50, 60\n")
 
-data = sales.get_all_values()
+    data_str = input("Enter your data here: ")
 
-print(data)
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into ints,
+    or if there are not exactly 6 values
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+            f"Exactly 6 values required, you have provided {len(values)}")
+    except ValueError as e:
+        print(f"Invalid data: {e}, Essayez-vous encore une fois\n")
+
+
+
+get_sales_data()
+    
